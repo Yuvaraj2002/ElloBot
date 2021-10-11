625df2fa0eaefe6d2c0cfde2acda90c9aeeb4966
Vibhav Deepak <70058308+VibeyVib@users.noreply.github.com>
2021-10-11 13:50:09 +0530

    initial frame

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import home from "./components/Home.js";
+import reactDom from "react-dom";
+function App() {
+  return (
+    <home/>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import reactDom from "react-dom";
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import App from "../App";
+import homecss from '../components/home.css';
+
+function home(){
+    return(
+        <div className="Header">
+            <h1>Hello World</h1>
+            
+        </div>
+    );
+}
+export default home;
